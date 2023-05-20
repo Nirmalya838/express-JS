7c842146deb434830d04a8519173f35f501d8753
Nirmalya838 <122223948+Nirmalya838@users.noreply.github.com>
2023-05-20 20:59:20 +0530

    body parser added

--- demo.js
+++ demo.js
@@ -1,19 +1,21 @@
 const http = require('http');
-
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next)=>{
-  console.log("in the middlewire!");
-  next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<html><form action="/product" method="POST"><input type="text" placeholder="item" name="title"/><button>add</button><input type="number" name="size" placeholder="size"/></form></html>');
 });
 
-app.use((req, res, next)=>{
-  console.log("asan hain!");
-  res.send('<h1>Hello There</h1>');
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from express!</h1>')
+});
 
 app.listen(3000);
