3883e07099f88a418f6577743ffa0d36520528cb
Nirmalya838 <122223948+Nirmalya838@users.noreply.github.com>
2023-05-22 15:54:07 +0530

    Group Chat App completed

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorRoutes = require('./routes/error');
+const loginRoutes = require('./routes/login');
+const msgRoutes = require('./routes/msg');
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
-app.use(errorRoutes);
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
+app.use('/login', loginRoutes);
+app.use('/',msgRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<p>Error 404</p><h3>Page not found</h3>');
+  });
+  
 
 app.listen(3000);
